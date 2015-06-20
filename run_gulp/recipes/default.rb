29d271d3e397e5ee840bd2ada0338f5ffd375c37
Merih Akar <merihakar@gmail.com>
2015-06-19 20:08:48 -0700

    let's try running deploy script only for static application types

--- run_gulp/recipes/default.rb
+++ run_gulp/recipes/default.rb
@@ -1,4 +1,9 @@
 node[:deploy].each do |application, deploy|
+    if deploy[:application_type] != 'static'
+        Chef::Log.debug("Skipping deploy::web application #{application} as it is not an static HTML app")
+        next
+    end
+
     script "npm and gulp" do
         interpreter "bash"
         user "root"
@@ -6,11 +11,13 @@ node[:deploy].each do |application, deploy|
         case node[:opsworks][:stack][:name]
         when 'zeplin-dev-stack'
             code <<-EOH
-                npm install && NODE_ENV=dev node ./node_modules/gulp/bin/gulp.js release
+                npm install
+                NODE_ENV=dev node ./node_modules/gulp/bin/gulp.js release
             EOH
         when 'zeplin-prod-stack'
             code <<-EOH
-                npm install && NODE_ENV=prod node ./node_modules/gulp/bin/gulp.js release
+                npm install
+                NODE_ENV=prod node ./node_modules/gulp/bin/gulp.js release
             EOH
         end
     end
