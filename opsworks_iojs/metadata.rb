a3670cc144d255cb41bde0adcbb253cbdebd57db
Merih Akar <merihakar@gmail.com>
2015-08-15 02:43:49 -0700

    remove from source and binary install options

--- opsworks_iojs/metadata.rb
+++ opsworks_iojs/metadata.rb
@@ -10,12 +10,9 @@ version '2.4.1'
 
 conflicts 'node'
 
-depends 'build-essential'
-depends 'ark'
 depends 'apt'
-depends 'homebrew'
 
-%w(debian ubuntu centos redhat smartos mac_os_x).each do |os|
+%w(debian ubuntu).each do |os|
   supports os
 end
 
