6780c354514ce855f012c739f40b44572c9fcd67
Merih Akar <merihakar@gmail.com>
2015-09-03 00:06:35 -0700

    remove deploy attributes

--- opsworks_iojs/attributes/deploy.rb
+++
@@ -1,5 +0,0 @@
-default[:deploy] = {}
-node[:deploy].each do |application, deploy|
-  default[:deploy][application][:deploy_to] = "/srv/www/#{application}"
-  default[:deploy][application][:nodejs][:restart_command] = "monit restart node_web_app_#{application}"
-  default[:deploy][application][:nodejs][:stop_command] = "monit stop node_web_app_#{application}"
