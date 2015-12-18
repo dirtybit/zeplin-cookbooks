ae107c67e33ba4633c734fb132e4dc0e3efa98d8
Muhammet Alay <muhammet@zeplin.io>
2015-12-18 11:50:59 +0200

    Fix the log line

--- apidoc/recipes/default.rb
+++ apidoc/recipes/default.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
     if deploy[:application_type] != 'nodejs'
-        Chef::Log.debug("Skipping deploy::application #{application} as it is not an static HTML app")
+        Chef::Log.debug("Skipping deploy::application #{application} as it is not a node.js app")
         next
     end
 
