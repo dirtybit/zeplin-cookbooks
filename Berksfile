d571b9be1d62cce7597ac943729b587c4d7a491a
ceylanismail <ceylanismail@gmail.com>
2016-04-30 02:08:45 +0300

    add mongodb locally

--- Berksfile
+++
@@ -1,8 +0,0 @@
-source "https://supermarket.getchef.com"
-
-cookbook 'yum-epel'
-cookbook 'build-essential'
-cookbook 'ark'
-cookbook 'apt'
-cookbook 'homebrew'
-cookbook 'mongodb'
---
+++ mongodb/metadata.rb
@@ -0,0 +1,7 @@
+name             'mongodb'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mongodb'
+long_description 'Installs/Configures mongodb'
+version          '0.1.0'
